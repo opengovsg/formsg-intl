da9832e152dcac5f98dfeb5a809f6486fa8d30a4
LoneRifle <LoneRifle@users.noreply.github.com>
2024-08-23 09:16:14 +0800

    chore(builder): hide MyInfo

---
+++ replacements/frontend/src/features/admin-form/create/builder-and-design/BuilderAndDesignDrawer/FieldListDrawer/FieldListDrawer.tsx
@@ -0,0 +1,78 @@
+import {
+  Box,
+  Divider,
+  Flex,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from '@chakra-ui/react'
+
+import { Tab } from '~components/Tabs'
+
+import { useCreatePageSidebar } from '~features/admin-form/create/common/CreatePageSidebarContext'
+
+import { useCreateTabForm } from '../../../builder-and-design/useCreateTabForm'
+import { CreatePageDrawerCloseButton } from '../../../common'
+import { FieldListTabIndex } from '../../constants'
+
+import { BasicFieldPanel, PaymentsInputPanel } from './field-panels'
+
+export const FieldListDrawer = (): JSX.Element => {
+  const { fieldListTabIndex, setFieldListTabIndex } = useCreatePageSidebar()
+  const { isLoading } = useCreateTabForm()
+
+  const tabsDataList = [
+    {
+      header: 'Basic',
+      component: BasicFieldPanel,
+      isHidden: false,
+      isDisabled: isLoading,
+      key: FieldListTabIndex.Basic,
+    },
+    {
+      header: 'Payments',
+      component: PaymentsInputPanel,
+      isHidden: false,
+      isDisabled: isLoading,
+      key: FieldListTabIndex.Payments,
+    },
+  ].filter((tab) => !tab.isHidden)
+
+  return (
+    <Tabs
+      pos="relative"
+      h="100%"
+      display="flex"
+      flexDir="column"
+      index={fieldListTabIndex}
+      onChange={setFieldListTabIndex}
+      isLazy
+    >
+      <Box pt="1rem" px="1.5rem" bg="white">
+        <Flex justify="space-between">
+          <Text textStyle="subhead-3" color="secondary.500" mb="1rem">
+            Fields
+          </Text>
+          <CreatePageDrawerCloseButton />
+        </Flex>
+        <TabList mx="-0.25rem" w="100%">
+          {tabsDataList.map((tab) => (
+            <Tab key={tab.key} isDisabled={tab.isDisabled}>
+              {tab.header}
+            </Tab>
+          ))}
+        </TabList>
+        <Divider w="auto" mx="-1.5rem" />
+      </Box>
+      <TabPanels pb="1rem" flex={1} overflowY="auto">
+        {tabsDataList.map((tab) => (
+          <TabPanel key={tab.key}>
+            <tab.component />
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  )
+}
